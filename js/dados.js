7a7fed81c9826700da1d166da5f3b9476e922424
Nadinael Silva <nadinael.silva@gmail.com>
2024-09-07 02:42:46 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,107 @@
+let profissionais = [
+    {
+        nome: "João da Carretinha",
+        bio: "Faço fretes para todo o Brasil. Tenho uma Carretinha e um corsinha. Chama aí",
+        contato: "81982386944",
+        servicos: "fretes",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Jurandir Filho",
+        bio: "Sou um podcaster renomado. Chama para os eventos e produções!",
+        contato: "81982386944",
+        servicos: "podcaster",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Joana Cardoso",
+        bio: "Sou dentista há 18 anos. Atendo adultos e crianças.",
+        contato: "81982386944",
+        servicos: "dentista",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    },
+    {
+        nome: "Ronaldo",
+        bio: "Sou um programador conhecido na cidade. Resolvo bugs e crio soluções. Chama aí!",
+        contato: "81982386944",
+        servicos: "suporte técnico, programador",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Carla Lima",
+        bio: "Sou cabeleireira especialista em cortes e coloração. Agende seu horário!",
+        contato: "81982386944",
+        servicos: "cabeleireira",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    },
+    {
+        nome: "Pedro Gomes",
+        bio: "Faço reformas gerais, desde encanamento até pintura. Profissional experiente.",
+        contato: "81982386944",
+        servicos: "pedreiro, pintor, encanador",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Juliana Silva",
+        bio: "Trabalho como personal trainer e ajudo pessoas a alcançarem seus objetivos de fitness.",
+        contato: "81982386944",
+        servicos: "personal trainer",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    },
+    {
+        nome: "Marcelo Pereira",
+        bio: "Ofereço serviços de eletricista. Instalação e manutenção elétrica com segurança.",
+        contato: "81982386944",
+        servicos: "eletricista",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Fernanda Costa",
+        bio: "Faço limpeza e organização de ambientes. Contrate para deixar sua casa impecável!",
+        contato: "81982386944",
+        servicos: "faxineira",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    },
+    {
+        nome: "Lucas Fernandes",
+        bio: "Ofereço serviços de jardinagem e paisagismo. Cuido do seu jardim como se fosse meu.",
+        contato: "81982386944",
+        servicos: "jardineiro",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Maria Oliveira",
+        bio: "Trabalho como cuidadora de idosos. Experiência e carinho para o cuidado que eles merecem.",
+        contato: "81982386944",
+        servicos: "cuidadora de idosos",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    },
+    {
+        nome: "Rodrigo Santos",
+        bio: "Faço pequenos reparos em eletrodomésticos e serviços de manutenção geral.",
+        contato: "81982386944",
+        servicos: "técnico em eletrodomésticos",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Aline Souza",
+        bio: "Ofereço serviços de costureira. Ajustes e confecção de roupas sob medida.",
+        contato: "81982386944",
+        servicos: "costureira",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    },
+    {
+        nome: "Felipe Araújo",
+        bio: "Realizo serviço de chaveiro, abro portas e troco fechaduras.",
+        contato: "81982386944",
+        servicos: "chaveiro",
+        perfil: "https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg"
+    },
+    {
+        nome: "Camila Farias",
+        bio: "Trabalho com maquiagem para eventos, noivas e dia a dia.",
+        contato: "81982386944",
+        servicos: "maquiadora",
+        perfil: "https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg"
+    }
+];
